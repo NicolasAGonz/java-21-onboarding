6947e39cf29dceff670901006e2055be92cf7c52
NicolasAGonz <nicolas.a.gonzalez@ar.ey.com>
2024-05-16 18:00:52 -0300

    add MS-Personas

---
+++ MS-Personas/src/test/java/com/dev/java/MSPersonas/MsPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.java.MSPersonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

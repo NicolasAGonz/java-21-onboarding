a11de1fc4597123e1a3d7daa583a1e10d7c4318d
NicolasAGonz <nicolas.a.gonzalez@ar.ey.com>
2024-05-17 15:29:42 -0300

    add MSCuentas y MSTarjetas

---
+++ MS-Tarjetas/src/test/java/com/dev/java/MSTarjetas/MsTarjetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.java.MSTarjetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTarjetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
